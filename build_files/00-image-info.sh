48cf4af6dab131ada22f16b5febad0dec0f89e1f
notworkflow <79778636+notworkflow@users.noreply.github.com>
2025-08-14 09:43:57 -0300

    Update 00-image-info.sh

--- build_files/00-image-info.sh
+++ build_files/00-image-info.sh
@@ -7,18 +7,12 @@ IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
 
 # image-info File
 sed -i 's/"image-name": [^,]*/"image-name": "'"$IMAGE_NAME"'"/' $IMAGE_INFO
+sed -i 's/"image-vendor": [^,]*/"image-vendor": "'"$IMAGE_VENDOR"'"/' $IMAGE_INFO
 sed -i 's|"image-ref": [^,]*|"image-ref": "'"$IMAGE_REF"'"|' $IMAGE_INFO
 
 # OS Release File
 sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
-
-# KDE About page
-# We don't want to edit an unexisting file on gnome variants
-if [[ "$IMAGE_NAME" != *gnome* ]]; then
-    sed -i "s|^Website=.*|Website=https://dev.bazzite.gg|" /etc/xdg/kcm-about-distrorc
-    if [[ "$IMAGE_NAME" != *nvidia* ]]; then
-        sed -i "s/^Variant=.*/Variant=Developer Experience/" /etc/xdg/kcm-about-distrorc
-    else
-        sed -i "s/^Variant=.*/Variant=Developer Experience (NVIDIA)/" /etc/xdg/kcm-about-distrorc
-    fi
-fi
+sed -i "s/^NAME=.*/NAME="'"Fp OS"'"/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME="'"Fp OS (From Bazzite)"'"/" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="'"fp-pc"'"/" /usr/lib/os-release
+sed -i "s/^HOME_URL=.*/HOME_URL="'"https://os.fpt.icu"'"/" /usr/lib/os-release
